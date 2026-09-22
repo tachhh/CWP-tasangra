bf70ce854fae3d1aceb4b418208dd37b57ba8f68
tachhh <tobtachinoez@gmail.com>
2026-09-22 21:56:36 +0700

    z

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,3 +1,3 @@
-first_name = "Tin"
-last_name = "Soungmek"
+first_name = "Tachin"
+last_name = "Sangrat"
 print(first_name,last_name)
--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,4 +1,4 @@
-first_name = "Tin"
-last_name = "Soungmek"
+first_name = "Tachin"
+last_name = "Sangrat"
 whole_name = first_name +" "+ last_name
 print(whole_name)
